4e5f653b77cce11c4bc883b00f8825406ccd5a10
Nich <nicholausdei2@gmail.com>
2024-03-21 03:23:03 +0700

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
+import {TiDelete} from 'react-icons/ti'
+import { BsFillCheckCircleFill } from 'react-icons/bs';
 
 function App() {
+  const [completedLayout, setCompleted] = useState(false);
+  const [ToDos,setTodos] = useState([]);
+  const [Title, setTitles] = useState("");
+  const [Description, setDescription] = useState("");
+  const [completedTodos, setCompletedTodos] = useState([]);
+  const DeleteCompletedTodos = index => {
+    let deletingToDos =[...completedTodos];
+    deletingToDos.splice(index);
+    localStorage.setItem('completedTodos',JSON.stringify(deletingToDos));
+    setCompletedTodos(deletingToDos);
+  }
+  const completedHandler = (index) =>{
+    let time = new Date();
+    let dd = time.getDate();
+    let mm = time.getMonth();
+    let yy = time.getFullYear();
+    let h = time.getHours();
+    let m = time.getMinutes();
+    let s = time.getSeconds();
+    let TimeFormat = dd + '/' + mm + '/' + yy + 'at ' + h + ':' +m + ':' +s;
+    let TimeCompleted = {
+      ...ToDos[index],
+      completedOn:TimeFormat
+    }
+    let ArrangeTimeCompleted = [...completedTodos];
+    ArrangeTimeCompleted.push(TimeCompleted);
+    setCompletedTodos(ArrangeTimeCompleted);
+    DeleteToDosHandler(index);
+    localStorage.setItem('completedTodos',JSON.stringify(ArrangeTimeCompleted));
+  }
+  const ToDosHandler =()=>{
+    let newToDos ={
+      title:Title,
+      description:Description
+    }
+    let ArrangeTodos =[...ToDos];
+    ArrangeTodos.push(newToDos);
+    setTodos(ArrangeTodos);
+    localStorage.setItem('todos',JSON.stringify(ArrangeTodos));
+  }
+  const DeleteToDosHandler =index=>{
+    let deletingToDos =[...ToDos];
+    deletingToDos.splice(index);
+    localStorage.setItem('todos',JSON.stringify(deletingToDos));
+    setTodos(deletingToDos);
+  }
+  useEffect(()=>{
+    let savedToDos = JSON.parse(localStorage.getItem('todos'));
+    let savedCompletedTodos = JSON.parse(localStorage.getItem('completedTodos'));
+    if (savedToDos){
+      setTodos(savedToDos)
+    }
+    if(savedCompletedTodos){
+      setCompletedTodos(savedCompletedTodos)
+    }
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>San's ToDoList</h1>
+      <div className="wrapper">
+        <div className="inputs">
+          <div className="inputing-items">
+            <label>Task</label>
+            <input type="text" value={Title} onChange={(e)=>setTitles(e.target.value)} placeholder="What tasks?"/>
+          </div>
+          <div className="inputing-items">
+            <label>Task Description</label>
+            <input type="text" value={Description} onChange={(e)=>setDescription(e.target.value)} placeholder="..."/>
+          </div>
+          <div className="inputing-items">
+            <button type="button" onClick={ToDosHandler} className="AddTaskButton">Add Task</button>
+          </div>
+        </div>
+        <div className="filters">
+         <button className={`filterButtons ${completedLayout===false&&'active'}`}onClick={()=>setCompleted(false)}>Incomplete</button>
+         <button className={`filterButtons ${completedLayout===true&&'active'}`}onClick={()=> setCompleted(true)}>Completed</button> 
+        </div>
+        <div className="todo-list">
+          {completedLayout===false&&ToDos.map((item,index)=>{
+            return(
+              <div className="todo-list-items"key={index}>
+               <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <div>
+                  <TiDelete className="icon-delete" onClick={()=>DeleteToDosHandler(index)} />
+                  <BsFillCheckCircleFill className="icon-check" onClick={()=>completedHandler(index)}/>
+                </div>
+              </div>
+            )
+          })}
+          {completedLayout===true&&completedTodos.map((item,index)=>{
+            return(
+              <div className="todo-list-items"key={index}>
+               <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                  <p><small>Completed on {item.completedOn}</small></p>
+                </div>
+                <div>
+                  <TiDelete className="icon-delete" onClick={()=>DeleteCompletedTodos(index)} />
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
